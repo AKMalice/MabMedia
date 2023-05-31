8cfc0cfd5bfd84a4a1e31275928f3a8963ec57a8
Abdul Nayeem Khalid <abdulnayeem2013@gmail.com>
2023-05-31 13:59:06 +0530

    Coming Soon Page

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900 h-screen">
+      
+      <div class="grid grid-cols-6">
+    <div className=" col-start-2 col-span-4 bg-gray-100 rounded-lg backdrop-filter backdrop-blur-lg bg-opacity-20 shadow-xl p-5 h-80 mt-40 ">
+    <p className="text-8xl font-thin font-sans text-center mt-5">MAB MEDIA GROUP</p>
+    <p className="text-2xl font-thin font-sans text-center mt-10">Promotional Gifts, Advertising and More</p>
+    <p className="text-xl font-thin font-sans text-center mt-5">Coming Soon...</p>
+    </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
